4adc40c028cfaae5bff443bb51b021ec87926856
quan_root <quan_root@163.com>
2018-08-20 19:18:25 +0800

    branch example

--- src/main/java/com/itguigu/git/Happy.java
+++ src/main/java/com/itguigu/git/Happy.java
@@ -5,5 +5,6 @@ public class Happy {
 	public static void main(String[] args) {
 		System.out.println("hello boy this this");
 		System.out.println("hello boy clone clone");
+		System.out.println("branch hot_hotfix");
 	}
 }
